9853298fbde10df818a8524c84c8172dca6f7be0
AnthnoyYY <yjh2332@gmail.com>
2017-07-27 00:08:00 +0800

    新增商品的评论功能

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Product, ProductService} from "../shared/product.service";
 import { Comment } from "../shared/product.service";
@@ -8,9 +8,12 @@ import { Comment } from "../shared/product.service";
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnChanges {
   public product: Product;
   public comments: Array<Comment>;
+  public newComment: string;
+  public showCommentAdder = false;
+  public newRating = 5;
   constructor(
     private productService: ProductService,
     private routerInfo: ActivatedRoute
@@ -20,6 +23,17 @@ export class ProductDetailComponent implements OnInit {
     const productId: number = this.routerInfo.snapshot.params['productId'];
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentForProduct(productId);
+  }
+
+  ngOnChanges (changes) {
+  }
+
+  addComment() {
+    this.comments.unshift(new Comment(0, this.product.id, new Date().toISOString(), 'jerry', this.newRating, this.newComment));
+    this.newComment = '';
+    this.newRating = 5;
+    this.product.rating = this.comments.reduce( (sum, comment) => sum += comment.rating , 0 ) / this.comments.length;
+    this.showCommentAdder = false;
   }
 
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,23 +1,36 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
 
   @Input()
-  public rating = 0;
-
+  public rating = 5;
+  @Input()
+  readonly: boolean = true;
+  @Output()
+  ratingChange: EventEmitter<number> = new EventEmitter();
   public stars: Array<boolean>;
-
   constructor() {}
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
     this.stars = [];
     for ( let i = 1; i <= 5; i++ ) {
       this.stars.push(i > this.rating);
+    }
+  }
+
+  changeRating(index) {
+    if ( !this.readonly ) {
+      this.rating = index + 1;
+      this.ratingChange.emit(this.rating);
+      this.ngOnInit();
     }
   }
 
